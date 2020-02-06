90d62c67c2c8000408e440f4cbd948f7fa5f9438
Ewelina Łabas <ewelina.labas@gmail.com>
2020-02-06 18:20:55 +0100

    Add moves log

--- src/App.js
+++ src/App.js
@@ -14,17 +14,29 @@ export const App = () => {
     return initialState
   };
 
+  const prepareCoordinates = (list, value, index) => {
+    return list.concat({
+      value: value,
+      row: index[0], 
+      col: index[1], 
+    })
+  }
+
   const [xIsNext, setXIsNext] = useState(true);
   const [winner, setWinner] = useState(null)
   const [stepNumber, setStepNumber] = useState(0)
   const [history, setHistory] = useState([generateBoardInitialState()])
+  const [coordinates, setCoordinates] = useState([])
   const nextPlayer = xIsNext ? 'X' : 'O';
 
   const handleClick = (index) => {
     const squareValuesNew = {...history[stepNumber]}
     squareValuesNew[index] = xIsNext ? 'X' : 'O'
+    setCoordinates(prepareCoordinates(coordinates, squareValuesNew[index], index))
     setStepNumber(stepNumber + 1)
-    const newHistory = history.slice(0, stepNumber + 1).concat(squareValuesNew)
+    const newHistory = history
+      .slice(0, stepNumber + 1)
+      .concat(squareValuesNew)
     setHistory(newHistory)
     setXIsNext(!xIsNext)
     setWinner(checkForWinner(squareValuesNew))
@@ -33,37 +45,65 @@ export const App = () => {
   const jumpTo = (step) => {
     setStepNumber(step)
     setXIsNext((step % 2 === 0))
+    setCoordinates(prepareCoordinates(coordinates, 'Go to move ' + step, ['-', '-']))
   }
 
   const moves = history.map((el, i) => {
     const description = i ? 'Go to move ' + i : 'Go to beginning'
       return (
-        <li>
+        <li key={i}>
           <button key={i} onClick={() => jumpTo(i)}>{description}</button>
         </li>
       )
   })
 
-  const message = (nextPlayer, winner) => {
+  const createMessage = (nextPlayer, winner) => {
     if(winner) {
       return <p>{winner + " won!"}</p>
     } else {
       return <p>{'Next player is: ' + nextPlayer}</p>
     }
   }
+  const message = createMessage(nextPlayer, winner)
+
+  const decoratedCoordinates = coordinates.map(el => {
+    return (
+      <tr>
+        <td>{el.value}</td>
+        <td>{el.row}</td>
+        <td>{el.col}</td>
+      </tr>
+    )
+  })
+      
 
   return (
     <div className="game">
       <div>
-        {message(nextPlayer, winner)}
+        {message}
         <Board 
           squareValues={history[stepNumber]}
           handleClick={handleClick}
         />
       </div>
         <div className="game-info">
-          <p>List of moves</p>
+          <p>History of moves</p>
           <ol>{moves}</ol>
+        </div>
+        <div className="game-info">
+          <p>Coordinates of moves</p>
+          <table>
+            <thead>
+              <tr>
+                  <th>Value</th>
+                  <th>Row</th>
+                  <th>Column</th>
+              </tr>
+            </thead>
+            <tbody>
+              {decoratedCoordinates}
+            </tbody>
+          </table>
         </div>
     </div>
   );
