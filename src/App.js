769c6f83ab9b3787e313ae32d08534b5d265079e
Ewelina Łabas <ewelina.labas@gmail.com>
2020-02-06 12:44:35 +0100

    Add history of moves

--- src/App.js
+++ src/App.js
@@ -14,31 +14,49 @@ export const App = () => {
     return initialState
   };
 
-  const [squareValues, setSquareValues] = useState(generateBoardInitialState());
   const [xIsNext, setXIsNext] = useState(true);
   const [winner, setWinner] = useState(null)
+  const [stepNumber, setStepNumber] = useState(0)
+  const [history, setHistory] = useState([generateBoardInitialState()])
   const nextPlayer = 'Next player: '.concat(xIsNext ? 'X' : 'O');
 
   const handleClick = (index) => {
-    const squareValuesNew = {...squareValues}
+    const squareValuesNew = {...history[history.length - 1]}
     squareValuesNew[index] = xIsNext ? 'X' : 'O'
-    setSquareValues(squareValuesNew)
+    setHistory([...history, squareValuesNew])
     setXIsNext(!xIsNext)
+    setStepNumber(history.length)
     setWinner(checkForWinner(squareValuesNew))
   };
+
+  const jumpTo = (step) => {
+    setStepNumber(step)
+    setXIsNext((step % 2 === 0))
+  }
+
+  const moves = history.map((el, i) => {
+    const description = i ? 'Go to move ' + i : 'Go to beginning'
+    if(history.length > 1) {
+      return (
+        <li>
+          <button key={i} onClick={() => jumpTo(i)}>{description}</button>
+        </li>
+      )
+    }
+  })
 
   return (
     <div className="game">
       <div>
         <Board 
-          squareValues={squareValues}
+          squareValues={history[stepNumber]}
           nextPlayer={nextPlayer}
           winner={winner}
           handleClick={handleClick}
         />
         <div className="game-info">
           <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     </div>
